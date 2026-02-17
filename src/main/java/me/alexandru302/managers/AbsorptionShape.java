2c0bf258ed0f84cb80c3fbdf46758b0f8bd2269d
BuildTools <unconfigured@null.spigotmc.org>
2026-02-17 08:57:26 +0200

    finishing touches 3 :sob:

--- src/main/java/me/alexandru302/managers/AbsorptionShape.java
+++ src/main/java/me/alexandru302/managers/AbsorptionShape.java
@@ -10,14 +10,14 @@ public enum AbsorptionShape {
 
     @NotNull
     public static AbsorptionShape fromConfig(String raw) {
-        if (raw == null || raw.isBlank()) return DIAMOND;
+        if (raw == null || raw.isBlank()) return CUBE;
 
         try {
             return AbsorptionShape.valueOf(raw.trim().toUpperCase());
         } catch (IllegalArgumentException exception) {
             BetterSponges.getInstance().getLogger().severe("Failed to get the absorption_shape config value:" + raw +
                     "Did you write it wrong or is it my fault");
-            return DIAMOND;
+            return CUBE;
         }
     }
 }
